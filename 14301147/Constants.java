2bd7990841218ae85e064eaa82d479e5f897029c
gEleve <14301147@bjtu.edu.cn>
2016-10-15 13:24:58 +0800

    Update Constants.java

--- 14301147/Constants.java
+++ 14301147/Constants.java
@@ -1,35 +1,8 @@
-import java.io.IOException;
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-public class LoginServlet implements Servlet{
-	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-	request.setCharacterEncoding("utf-8");
-    String username = request.getParameter("username");
-    String password = request.getParameter("paw");
-    System.out.println(username+password);
-   }
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+import java.io.File;
 
-	@Override
-	public ServletConfig getServletConfig() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public String getServletInfo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void init(ServletConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
+public class Constants {
+    public static final String WEB_ROOT = System.getProperty("user.dir")
+            + File.separator + "webroot";
+   
+            
 }
